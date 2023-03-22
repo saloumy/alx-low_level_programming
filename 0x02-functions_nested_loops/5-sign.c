6a8b7a204bfc1546cc6d35c62b251285eaa51436
saloumy <essadek92@gmail.com>
2023-03-22 07:56:45 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-			_putchar('+');
+		_putchar('+');
 			return (1);
 	} else if (n == 0)
 	{
-			_putchar(48);
+		_putchar(48);
 			return (0);
 	} else if (n < 0)
 	{
-			_putchar('-');
+		_putchar('-');
 	}
 		return (-1);
 }
